b9b6b68a5c322ff0d6a7c8cab987bbadfbdadec9
joschout <schouterden_jonas@hotmail.com>
2015-03-19 23:56:09 +0100

    lots of sysouts

--- src/graphProcessing/SubgraphManager.java
+++ src/graphProcessing/SubgraphManager.java
@@ -25,18 +25,37 @@ public class SubgraphManager {
 		
 		//PREPROCESSING STEP
 		Map<Double, Set<HashTag>> degreeVerticesMap = new HashMap<Double, Set<HashTag>>();	
+		
 		for (HashTag vertex : inputGraph.vertexSet()){
+			System.out.println("//================//");
+//			System.out.println("current keyset: " + degreeVerticesMap.keySet().toString());
+//			System.out.println("Current valueset: " + degreeVerticesMap.values().toString());
+			
+//			System.out.println("vertex to be put in the map: "+ vertex .toString());
 			double degreeOfVertex = densitymanager.getDegreeOfVertex(vertex, inputGraph);
+//			System.out.println("degree of the vertex: " + degreeOfVertex);
 			if(degreeVerticesMap.containsKey(degreeOfVertex)){
-				degreeVerticesMap.get(degreeVerticesMap).add(vertex);
+//				System.out.println("the map contains the key/ vertices of this degree");	
+//				System.out.println("a print of the current map: "+degreeVerticesMap.toString());
+				
+				degreeVerticesMap.get(degreeOfVertex).add(vertex);
 			}else{
+//				System.out.println("the map does not contain the key/ vertices of this degree");
 				Set<HashTag> setForThisDegree = new HashSet<HashTag>();
 				setForThisDegree.add(vertex);
 				degreeVerticesMap.put(degreeOfVertex, setForThisDegree);
-			}	
+			}
+//			System.out.println("//================//");
 		}
 		
+		/*
+		 * STOPCONDITIE KAN FOUT ZIJN AAAAAAAAARRRGGGHHH
+		 */
+		
 		while(!degreeVerticesMap.isEmpty()){
+			System.out.println("#START OF WHILE#");
+			System.out.println("current keyset: " + degreeVerticesMap.keySet().toString());
+			System.out.println("Current valueset: " + degreeVerticesMap.values().toString());
 			
 			//get the smallest degree
 			double tempMin = Double.MAX_VALUE;
@@ -46,12 +65,18 @@ public class SubgraphManager {
 				}
 			}
 			
+			System.out.println("smallest degree is: " + tempMin);
+			
 			//get 1 node with the smallest degree
-			Set<HashTag> verticesOfSmallestDegree =degreeVerticesMap.get(tempMin);
+			Set<HashTag> verticesOfSmallestDegree = degreeVerticesMap.get(tempMin);
+			System.out.println("de set van vertices met deze degree is: "+ verticesOfSmallestDegree.toString());
 			HashTag vertex = (HashTag) verticesOfSmallestDegree.toArray()[0];
+			System.out.println("de vertex met deze degree is: " + vertex.toString());
 			verticesOfSmallestDegree.remove(vertex);
+			System.out.println("this vertex is removed!");
 			if(verticesOfSmallestDegree.isEmpty()){
 				degreeVerticesMap.remove(tempMin);
+				System.out.println("map for this degree removed!");
 			}
 			
 			//remove v and all its edges
@@ -66,8 +91,12 @@ public class SubgraphManager {
 			if (densityOfTempGraph > densityOfTempDensestSubgraph) {
 				tempDensestSubgraph = (SimpleWeightedGraph<HashTag, DefaultWeightedEdge>) tempGraph.clone();
 			}
+			
+			System.out.println("#END OF WHILE#");
+			//check if the graph has at least 3  
 		}
 		
+		System.out.println("the method returns");
 		return tempDensestSubgraph;
 	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,18 @@
 package main;
 
+import graphProcessing.DensityManager;
+import graphProcessing.SubgraphManager;
+import graphProcessing.WeightedDensityManager;
+import gui.GUIApplet;
+
 import java.io.IOException;
 
+import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleWeightedGraph;
 
 import preprocessing.Filter;
 import preprocessing.JsonParser;
+import tweets.HashTag;
 import tweets.Tweet;
 
 public class Main {
@@ -13,13 +20,25 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 
 		JsonParser parser = new JsonParser("data");
-		Filter filter = new Filter(parser.parseFile("NewYork-2015-2-23"));
-		SimpleWeightedGraph graph = filter.createWeightedGraph();
+		Filter filter = new Filter(parser.parseFile("test"));
+		SimpleWeightedGraph<HashTag, DefaultWeightedEdge> graph = filter.createWeightedGraph();
 		
-		for(Tweet tweet : filter.tweets){
-			//System.out.print(tweet + "\n-----------------------------\n");
+//		for(Tweet tweet : filter.tweets){
+//			//System.out.print(tweet + "\n-----------------------------\n");
+//		}
+		
+		WeightedDensityManager<HashTag, DefaultWeightedEdge> densityManager = new WeightedDensityManager<HashTag, DefaultWeightedEdge>();
+		SubgraphManager subgraphManager = new SubgraphManager();
+		SimpleWeightedGraph<HashTag, DefaultWeightedEdge> densestSubgraph = subgraphManager.getDensestSubgraph(graph, densityManager);
+		
+		System.out.println("vertexset: "+densestSubgraph.vertexSet().toString());
+		
+		for(HashTag vertex: densestSubgraph.vertexSet()){
+			System.out.println(vertex.toString() +": " + densityManager.getDegreeOfVertex(vertex, densestSubgraph));
 		}
-		
+		for(DefaultWeightedEdge edge: densestSubgraph.edgeSet()){
+			System.out.println("edges: " +edge.toString());
+		}
 	}
 
 }

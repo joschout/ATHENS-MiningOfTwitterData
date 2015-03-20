a9bbb81d937cf0416a7ef0a6bfe88ce5906f586a
joschout <schouterden_jonas@hotmail.com>
2015-03-20 10:32:16 +0100

    subgraphmanager is changed to include a circularfifoqueue

--- src/graphProcessing/SubgraphManager.java
+++ src/graphProcessing/SubgraphManager.java
@@ -13,10 +13,31 @@ import tweets.HashTag;
 
 public class SubgraphManager {
 
-	
+	/**
+	 * Constructor using as a default a CircularFifoQueue of size 10
+	 */
 	public SubgraphManager(){
+		this.densestSubgraphQueue = new CircularFifoQueue<SimpleWeightedGraph<HashTag,DefaultWeightedEdge>>(10);
 	}
 	
+	/**
+	 * 
+	 * @param numberOfDensestSubgraphsToBeSaved the number of densest subgraphs that are possible to be saved in a CircularFifoQueue
+	 */
+	public SubgraphManager(int numberOfDensestSubgraphsToBeSaved){
+		this.densestSubgraphQueue = new CircularFifoQueue<SimpleWeightedGraph<HashTag,DefaultWeightedEdge>>(numberOfDensestSubgraphsToBeSaved);
+	}
+
+	/**
+	 * Queue that can contain at max a specific number of elements. The queue is circular First-In, First-Out,
+	 *  so when it is full and a new element is added, the oldest element in the queue will be removed.
+	 */
+	private CircularFifoQueue<SimpleWeightedGraph<HashTag, DefaultWeightedEdge>> densestSubgraphQueue;
+	
+	public CircularFifoQueue<SimpleWeightedGraph<HashTag, DefaultWeightedEdge>> getDensestSubgraphQueue() {
+		return densestSubgraphQueue;
+	}
+
 	public SimpleWeightedGraph<HashTag, DefaultWeightedEdge> getDensestSubgraph(SimpleWeightedGraph<HashTag, DefaultWeightedEdge> inputGraph,
 			DensityManager<HashTag, DefaultWeightedEdge> densitymanager){
 		
@@ -194,7 +215,6 @@ public class SubgraphManager {
 		TreeMap<Double, Set<HashTag>> degreeVerticesMap = new TreeMap<Double, Set<HashTag>>();	
 		
 		for (HashTag vertex : inputGraph.vertexSet()){
-			System.out.println("//================//");
 //			System.out.println("current keyset: " + degreeVerticesMap.keySet().toString());
 //			System.out.println("Current valueset: " + degreeVerticesMap.values().toString());
 			
@@ -259,5 +279,83 @@ public class SubgraphManager {
 		
 		System.out.println("the method returns");
 		return tempDensestSubgraph;
+	}
+	
+	public CircularFifoQueue<SimpleWeightedGraph<HashTag, DefaultWeightedEdge>> getDensestSubgraph4(SimpleWeightedGraph<HashTag, DefaultWeightedEdge> inputGraph,
+			DensityManager<HashTag, DefaultWeightedEdge> densitymanager){
+		
+		
+		SimpleWeightedGraph<HashTag, DefaultWeightedEdge> tempGraph = (SimpleWeightedGraph<HashTag, DefaultWeightedEdge>) inputGraph.clone();
+		SimpleWeightedGraph<HashTag, DefaultWeightedEdge> tempDensestSubgraph = (SimpleWeightedGraph<HashTag, DefaultWeightedEdge>) inputGraph.clone();
+			
+		//PREPROCESSING STEP
+		TreeMap<Double, Set<HashTag>> degreeVerticesMap = new TreeMap<Double, Set<HashTag>>();	
+		
+		for (HashTag vertex : inputGraph.vertexSet()){
+//			System.out.println("current keyset: " + degreeVerticesMap.keySet().toString());
+//			System.out.println("Current valueset: " + degreeVerticesMap.values().toString());
+			
+//			System.out.println("vertex to be put in the map: "+ vertex .toString());
+			double degreeOfVertex = densitymanager.getDegreeOfVertex(vertex, inputGraph);
+//			System.out.println("degree of the vertex: " + degreeOfVertex);
+			if(degreeVerticesMap.containsKey(degreeOfVertex)){
+	//				System.out.println("the map contains the key/ vertices of this degree");	
+	//				System.out.println("a print of the current map: "+degreeVerticesMap.toString());
+					
+					degreeVerticesMap.get(degreeOfVertex).add(vertex);
+			}else{
+	//				System.out.println("the map does not contain the key/ vertices of this degree");
+					Set<HashTag> setForThisDegree = new HashSet<HashTag>();
+					setForThisDegree.add(vertex);
+					degreeVerticesMap.put(degreeOfVertex, setForThisDegree);
+			}
+			
+//			System.out.println("//================//");
+		}
+		
+		/*
+		 * STOPCONDITIE KAN FOUT ZIJN AAAAAAAAARRRGGGHHH
+		 */
+		
+		while(!degreeVerticesMap.isEmpty()){
+			System.out.println("#START OF WHILE#");
+			System.out.println("current keyset: " + degreeVerticesMap.keySet().toString());
+			System.out.println("Current valueset: " + degreeVerticesMap.values().toString());
+			
+			//get the smallest degree
+			double tempMin = degreeVerticesMap.firstKey();
+			
+			System.out.println("smallest degree is: " + tempMin);
+			
+			//get 1 node with the smallest degree
+			Set<HashTag> verticesOfSmallestDegree = degreeVerticesMap.get(tempMin);
+			System.out.println("de set van vertices met deze degree is: "+ verticesOfSmallestDegree.toString());
+			for(HashTag vertex: verticesOfSmallestDegree){
+				System.out.println("de vertex met deze degree is: " + vertex.toString());
+				tempGraph.removeVertex(vertex);
+				System.out.println("this vertex is removed!");
+			}
+			
+			degreeVerticesMap.remove(tempMin);
+			System.out.println("map for this degree removed!");
+			
+			
+			/*
+			 * if the density of tempGraph is larger than the density of tempDensestSubgraph
+			 * then store tempGraph as tempDensestSubgraph
+			 */
+			double densityOfTempGraph = densitymanager.getDensity(tempGraph);
+			double densityOfTempDensestSubgraph = densitymanager.getDensity(tempDensestSubgraph);
+			if (densityOfTempGraph > densityOfTempDensestSubgraph) {
+				tempDensestSubgraph = (SimpleWeightedGraph<HashTag, DefaultWeightedEdge>) tempGraph.clone();
+				getDensestSubgraphQueue().add(tempDensestSubgraph);
+			}
+			
+			System.out.println("#END OF WHILE#");
+			//check if the graph has at least 3  
+		}
+		
+		System.out.println("the method returns");
+		return getDensestSubgraphQueue();
 	}
 }
